d2992b2c285fd259e61da13e5f65b6a224023eb5
Homero <homerao@gmail.com>
2024-12-02 08:43:33 -0300

    subindo o exercicio

---
+++ gruntfile.js
@@ -0,0 +1,74 @@
+const {option} = require('grunt');
+
+module.exports = function (grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        less: {
+            development: {
+                files: {
+                    'dev/styles/main.css':'src/styles/main.less'
+                }
+            },
+            production : {
+                options: {
+                    compress:true
+                }, files: {
+                    'dist/styles/main.min.css': 'src/styles/main.less'
+                }
+            }
+        }, replace: {
+            dev: {
+                 options: {
+                 patterns:[
+                {
+                    match: 'ENDERECO_DO_CSS',
+                    replacement: './styles/main.css'
+                },
+                {
+                    match: 'ENDERECO_DO_JS',
+                    replacement: '../src/scripts/main.js'
+                }
+            ]}, 
+            files: [
+                {
+                    expand: true,
+                    flatten: true,
+                    src:['src/index.html'],
+                    dest: 'dev/'
+                }
+            ]
+            
+        }, 
+        dist: {
+            options: {
+            patterns:[
+           {
+               match: 'ENDERECO_DO_CSS',
+               replacement: './styles/main.min.css'
+           },
+           {
+            match: 'ENDERECO_DO_JS',
+            replacement: './scripts/main.min.js'
+        }
+       ]}, 
+       files: [
+           {
+               expand: true,
+               flatten: true,
+               src:['pre-build/index.html'],
+               dest: 'dist/'
+           }
+       ]
+       
+   }
+    },
+    uglify: {
+        target:{
+            files: {
+                'dist/scripts/main.min.js' :'src/scripts/main.js'
+            }
+        }
+    }
+    })
+    
+}
